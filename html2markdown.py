0f53a2ba088f0b88f4769aa53302f132aab57364
Edvard Rejthar <edvard.rejthar@nic.cz>
2016-12-15 20:08:46 +0100

    integrated MHTML autounpacking

--- html2markdown.py
+++ html2markdown.py
@@ -4,6 +4,8 @@ import re, json, ipdb, sys, os, traceback
 from bs4 import BeautifulSoup as bs
 from argparse import ArgumentParser
 from contextlib import contextmanager
+from subprocess import run, DEVNULL
+from time import time
 __help__ = """Converts html (output from OneNote) to Zim format."""
 
 @contextmanager
@@ -15,7 +17,7 @@ def d():
         tb= traceback.print_exc()        
         ipdb.post_mortem(tb)
 
-class Html2Zim:           
+class Html2Markdown:           
     def _getFormat(self, el):                        
         def _check(el, field):                               
             if not hasattr(el,"matches") or el.matches is None:
@@ -49,7 +51,9 @@ class Html2Zim:
                 continue
 
             if accords and "name" in par:
-                accords = (el.parent.name == par["name"])          
+                accords = (el.parent.name == par["name"])
+            if accords and "parent-name" in par:
+                accords = (el.parent.parent.name == par["parent-name"])          
 
             if not accords:
                 continue            
@@ -63,7 +67,7 @@ class Html2Zim:
         #    for ch in el.children:                
         #        self.loopEl(ch)              
         #    return  
-        #if "spadv" in el: import ipdb; ipdb.set_trace()
+        #if "Microso" in el: import ipdb; ipdb.set_trace()
                    
         el.sout = el.replace("\n", " ")            
         el.sout = re.sub('\s+', ' ', el.sout) #.strip()                                                    
@@ -114,6 +118,8 @@ class Html2Zim:
          
         if el.definition == "anchor":
             self._buffer += (el.form).format(el.parent.attrs["href"], el.sout)
+        elif el.definition == "stupid-nested-anchor":
+            self._buffer += (el.form).format(el.parent.parent.attrs["href"], el.sout)
         elif el.definition:        
             self._buffer += (el.form).format(el.sout) 
         elif el.findParent("li"):
@@ -128,7 +134,7 @@ class Html2Zim:
     def _saveBuffer(self):
         self._buffer = self._buffer.strip()
         if self._buffer:            
-            fn = os.path.dirname(self.formatted_file) + "/" + self._bufferName.replace(" ","_") + "." + self.extension
+            fn = self.saveDir + self._bufferName.replace(" ","_") + "." + self.extension
             #if os.path.isfile(fn) and fn not in self.createdFiles: print("Warning: File {} already exists, overwrite".format(fn))            
             with open(fn, "a" if fn in self.createdFiles else "w") as f:
                 # for each header, we create a file but there might be multiple identical headers in the formatted file -> append
@@ -145,7 +151,10 @@ class Html2Zim:
         self._bufferName = os.path.splitext(os.path.basename(formatted_file))[0] #"out"
         self._buffer = ""        
         self.lastTd = None
-        self.lastTr = None
+        self.lastTr = None                
+        self.saveDir = os.path.dirname(os.path.realpath(self.formatted_file)) + "/"
+        
+        self._checkMht()
         
         try:
             with open(self.formatted_file) as f: #, encoding="ISO-8859-1"
@@ -160,7 +169,7 @@ class Html2Zim:
         for el in soup.findAll(text=True):                        
             if el.strip("\n") == "":
                 continue
-            if el == "Created with Microsoft OneNote 2010\nOne place for all your notes and information":
+            if el.parent and el.parent.text == "Created with Microsoft OneNote 2010\nOne place for all your notes and information":
                 continue        
             
             el.myContext = el.findParent("li") or el.findParent("p") or el.findParent("div")
@@ -177,6 +186,39 @@ class Html2Zim:
         
         self._saveBuffer()
         #print(self._buffer)
+    
+    def _checkMht(self):
+        if os.path.splitext(args.file)[1] in [".mht",".mhtml"]: # we should unpack MHT
+            try: 
+                os.mkdir("/tmp/mhtifier")
+            except FileExistsError:
+                pass
+            mhtdir = "/tmp/mhtifier/"+os.path.basename(args.file) + "_" + str(time())    
+            sys.argv = ["external-import-hack", "--unpack", args.file, mhtdir]
+            from lib.mhtifier import main as Mhtifier        
+            Mhtifier()            
+            if not os.path.exists(mhtdir):
+                print("Can't unpack MHT to {}".format(mhtdir))
+                quit()
+                
+            # loop unpacked files
+            found = []
+            for root, _, files in os.walk(mhtdir):
+                for file in files:
+                    if ".htm" in file:
+                        found.append(root + '/' + file)
+                        
+            # change formatted_file to current unpacked htm file
+            if len(found) == 0:
+                print("No files unpacked from MHT.")
+                quit()
+            elif len(found) > 1:
+                print("Multiple files unpacked from MHT: {}. Not implented what to do now.".format(found))
+                quit()
+            else:
+                self.formatted_file = found[0]                
+                print("File to be reformatted: {}".format(self.formatted_file))
+        
         
 if __name__ == "__main__":    
     parser = ArgumentParser()    
@@ -187,10 +229,10 @@ if __name__ == "__main__":
         print(__help__)
         parser.print_help()
         quit()    
-    res = parser.parse_args()
-        
+    args = parser.parse_args()
+                                 
     # load definition file
-    if res.zim: 
+    if args.zim: 
         file, ext = "zim", "txt"    
     else: 
         file, ext = "markdown", "md" # markdown is default
@@ -200,4 +242,4 @@ if __name__ == "__main__":
          quit()        
 
     # launch reformatting
-    Html2Zim(def_file, res.file, ext)
+    Html2Markdown(def_file, args.file, ext)    
